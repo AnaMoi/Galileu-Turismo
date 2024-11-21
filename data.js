a9018696c6b86b3dde9611a74356deffb94b1d6b
AnaMoi <moianaflavia@gmail.com>
2024-11-21 19:42:41 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,78 @@
+export const data = [
+    {
+        "titulo": "Praça da Independência",
+        "descricao": "A Praça da Independência é um marco histórico da cidade, rodeada por grandes arquiteturas e  possui uma bela fonte",
+        "imagem": "images/praca.png",
+        "categorias": ["Histórico", "Cultural", "Igrejas"]
+    },
+    {
+        "titulo": "Vinícola Guaspari",
+        "descricao": " Grande vinícola que oferece uma experiência de visitas guiadas única pelos vinhedos e degustação de vinhos imperdivél.",
+        "imagem": "images/foto guaspari.jpg",
+        "categorias": ["Ecoturismo", "Degustação Sensorias","Pousada","Vinhos"]
+    },
+   
+  
+    {
+        "titulo": "Apiário Maria Mel",
+        "descricao": "Uma vivência estraordinária sobre criação de abelhas e produção de mel",
+        "imagem": "images/apiario maria.png",
+        "categorias": ["Natureza"]
+      },
+      {
+        "titulo": "Palácio do Café",
+        "descricao": "Um símbolo da história do café em Espírito Santo do Pinhal, inalgurado em 1893,atualmente sede do governo municipal e do Centro Cultural.",
+        "imagem": "images/palacio cafe.png",
+        "categorias": ["Histórico", "Cultural"]
+      },
+      {
+        "titulo": "Fazenda Mocoquinha",
+        "descricao": "Oportunidade espetacúlar de trilhas ecológicas e preserva árvores milenares.",
+        "imagem": "images/mocoquinha.png",
+        "categorias": ["Natureza", "Ecoturismo"]
+      },
+      {
+        "titulo": "Casarão 175",
+        "descricao": "Arquitetura do final do século XIX e atualmente funciona como um centro cultural, construído em 1903.",
+        "imagem": "images/casarao-175.jpg",
+        "categorias": ["Histórico", "Arquitetura"]
+      },
+      {
+        "titulo": "Rancho Rio Manso",
+        "descricao": "Espaço rural que oferece lazer em contato com a natureza, com incríveis passeios.",
+        "imagem": "images/rio manso.png",
+        "categorias": ["Natureza", "Ecoturismo"]
+      },
+      {
+        "titulo": "Cristo Redentor",
+        "descricao": "Com 11 metros de altura, inspirada no Cristo Redentor do Rio de Janeiro, é uma marco religioso da cidade.",
+        "imagem": "images/cristo.png",
+        "categorias": ["Religioso"]
+      },
+      {
+        "titulo": "Santa Luiza",
+        "descricao": "Em louvor a Santa padroeira dos olhos, foi inaugurado em 13 de dezembro de 1960.",
+        "imagem": "images/santa luiza.png",
+        "categorias": ["Religioso"]
+      },
+      {
+        "titulo": "Inverno D' Itália",
+        "descricao": " Uma rede de cafeterias pinhalense, focada nos cafés especiais de Espírito Santo do Pinhal, com deliciosos doces e salgados produzidos em confeitaria própria.",
+        "imagem": "images/inverno de italia.png",
+        "categorias": ["Degustação Sensorias", "Cafeteria"]
+      },
+      {
+        "titulo": "Casa do Ipê",
+        "descricao": "Uma chácara dentro da cidade que abriga o segundo vinhedo mais antigo de Pinhal, além de sua culinária impecavél.",
+        "imagem": "images/casa do ipe.png",
+        "categorias": ["Degustação Sensorias","Vinhos"]
+      },
+      {
+        "titulo": "Toca do Kaynã",
+        "descricao": "Fazenda localizada aos pés da Serra da Mantiqueira paulista, com experiências sem iguais.",
+        "imagem": "images/toca kayna.png",
+        "categorias": ["Ecoturismo","Degustação Sensorias"]
+      },
+]
+
+
